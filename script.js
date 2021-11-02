5f8bff801be557ef631e249a4a0a7a1ce1eba8f2
Keljrack <66731544+keljrackdevs@users.noreply.github.com>
2021-11-02 17:28:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function SetUpGames() {
   AddProject("Hextris",          "img/Hextris.png",       "Projects/Hextris/index.html");
-  AddProject("2048",             "img/2048.PNG,          "Projects/2048/index.html");
+  AddProject("2048",             "img/2048.PNG",           "Projects/2048/index.html");
   AddProject("Tetris",           "img/Tetris.jpg",        "Projects/Tetris/index.html");
   AddProject("Cookie Clicker",   "img/CookieClicker.jpg", "Projects/Cookie Clicker/index.html");
-  AddProject("Flappy Bird",      "img/FlappyBird.jpeg",    "Projects/FlappyBird/index.html");
+  AddProject("Flappy Bird",      "img/FlappyBird.jpeg",   "Projects/FlappyBird/index.html");
   AddProject("Javascript Racer", "img/JsRacer.png",       "Projects/JsRacer/index.html");
   AddProject("Hit The Target",   "img/HitTheTarget.png",  "Projects/HitTheTarget/index.html");
   AddProject("Snake",            "img/Snake.png",         "Projects/Snake/index.html");
