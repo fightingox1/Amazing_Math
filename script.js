e3b3bdcfe9c2a917a281fb2cc75e3ae38de185df
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2021-11-02 22:05:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function SetUpGames() {
-  AddProject("Hextris",          "img/Hextris.png",       "Projects/Hextris/index.html");
-  AddProject("2048",             "img/2048.png",           "Projects/2048/index.html");
-  AddProject("Tetris",           "img/Tetris.jpg",        "Projects/Tetris/index.html");
-  AddProject("Cookie Clicker",   "img/CookieClicker.jpg", "Projects/Cookie Clicker/index.html");
-  AddProject("Flappy Bird",      "img/FlappyBird.jpeg",   "Projects/FlappyBird/index.html");
-  AddProject("Javascript Racer", "img/JsRacer.png",       "Projects/JsRacer/index.html");
-  AddProject("Hit The Target",   "img/HitTheTarget.png",  "Projects/HitTheTarget/index.html");
-  AddProject("Snake",            "img/Snake.png",         "Projects/Snake/index.html");
+  AddProject("Hextris",          "img/Hextris.png",            "Projects/Hextris/index.html");
+  AddProject("2048",             "img/2048.png",               "Projects/2048/index.html");
+  AddProject("Tetris",           "img/Tetris.jpg",             "Projects/Tetris/index.html");
+  AddProject("Cookie Clicker",   "img/CookeClickTins.jpg", "Projects/Cookie Clicker/index.html");
+  AddProject("Flappy Bird",      "img/FlappyBird.jpeg",        "Projects/FlappyBird/index.html");
+  AddProject("Javascript Racer", "img/JsRacer.png",            "Projects/JsRacer/index.html");
+  AddProject("Hit The Target",   "img/HitTheTarget.png",       "Projects/HitTheTarget/index.html");
+  AddProject("Snake",            "img/Snake.png",              "Projects/Snake/index.html");
 
   LoadAnnouncment("Test", "Hello I was a bit bored on tuesday and decided to create this announcment system. I dont know if il use this a lot but it is here. When you close this it should be gone forever. If you get the same announcment twice than I need to know because it means that my system is not working");
 }
