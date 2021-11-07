7f4035ee1ac855e24aa56d9ea5484334db3f68b3
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2021-11-07 13:44:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function SetUpGames() {
-  AddProject("Hextris",          "img/Hextris.png",       "Projects/Hextris/index.html");
-  AddProject("2048",             "img/2048.png",          "Projects/2048/index.html");
-  AddProject("Tetris",           "img/Tetris.jpg",        "Projects/Tetris/index.html");
-  AddProject("Chrome Dino Game", "img/DinoGame.png",          "Projects/DinoGame/index.html");
+  AddProject("Hextris",          "img/Hextris.png",        "Projects/Hextris/index.html");
+  AddProject("2048",             "img/2048.png",           "Projects/2048/index.html");
+  AddProject("Tetris",           "img/Tetris.jpg",         "Projects/Tetris/index.html");
+  AddProject("Chrome Dino Game", "img/DinoGame.png",       "Projects/DinoGame/index.html");
   
-  AddProject("Flappy Bird",      "img/FlappyBird.jpeg",   "Projects/FlappyBird/index.html");
-  AddProject("Snake",            "img/Snake.png",         "Projects/Snake/index.html");
+  AddProject("Flappy Bird",      "img/FlappyBird.jpeg",    "Projects/FlappyBird/index.html");
+  AddProject("Snake",            "img/Snake.png",          "Projects/Snake/index.html");
   AddProject("Pac Man",          "img/PacGame.jpg",        "Projects/PacMan/index.html");
-  AddProject("Javascript Racer", "img/JsRacer.png",       "Projects/JsRacer/index.html");
+  AddProject("Javascript Racer", "img/JsRacer.png",        "Projects/JsRacer/index.html");
   
-  AddProject("Space Company",    "img/SpaceCompany.png",  "Projects/SpaceCompany/index.html");
+  AddProject("Space Company",    "img/SpaceCompany.png",   "Projects/SpaceCompany/index.html");
   AddProject("Cookie Clicker",   "img/CookeClickTins.jpg", "Projects/Cookie Clicker/index.html");
-  AddProject("Hit The Target",   "img/HitTheTarget.png",  "Projects/HitTheTarget/index.html");
-  AddProject("Doge The Lava",    "img/DogeLava.png",      "Projects/DogeLava/index.html");
+  AddProject("Hit The Target",   "img/HitTheTarget.png",   "Projects/HitTheTarget/index.html");
+  AddProject("Doge The Lava",    "img/DogeLava.png",       "Projects/DogeLava/index.html");
   
   LoadAnnouncment("ChangeLog5", "Change Log 0.5", "Bonjour, new games have been added to the website including a new clicker game called Space Company. It has some very simple UI but overall I have found it to be a fun game and it kept my attention for more than 20 mins. Other than Space Company I added in Pac-Man, The Dino Game, and Doge the Lava");
 }
