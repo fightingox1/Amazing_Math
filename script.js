6f5f86889eaa1531cd026993585227a6044e6f06
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2021-11-12 11:25:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function SetUpGames() {
   AddProject("Hit The Target",   "img/HitTheTarget.png",   "Projects/HitTheTarget/index.html");
   AddProject("Doge The Lava",    "img/DogeLava.png",       "Projects/DogeLava/index.html");
   
-  LoadAnnouncment("ChangeLog52", "Change Log 0.52", "The website has recently been updated to the Christmas theme! With christmas theme there are a couple of new things, a countdown to christmas at the top of the page, and a mute/umute button that plays christmas music! We also added some snowflakes falling, and we plan on adding a string of christmas lights at the top of the page. Merry Christmas!");
+  LoadAnnouncment("ChangeLog53", "Change Log 0.52", "The website has recently been updated to the Christmas theme! With christmas theme there are a couple of new things, a countdown to christmas at the top of the page. Merry Christmas!");
 }
 
 var PathToLoad;
